4636ede0f6e1d025391733936952d05c4a43edb9
LuisBarrios01 <67762297+LuisBarrios01@users.noreply.github.com>
2020-07-02 19:34:15 -0500

    Add files via upload

---
+++ js/codigo.js
@@ -0,0 +1,10 @@
+function probar()
+{
+    alert("Comprobar la union")
+}
+function Contradado()
+{
+   var n01 = document.getElementById('n1').value;
+   var C = (sueldo*0.11) 
+   document.getElementById('Contratado').innerHTML = C;
+}
